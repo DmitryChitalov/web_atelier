210791fafd68345dd0617937acb340681e798125
Dmitry Chitalov <cdi9@yandex.ru>
2019-11-26 16:31:55 +0500

    ready

---
+++ django2/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!h:\geekbrains\python\iiiq\djangoframework_2\homework\1\myshop\django2\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
